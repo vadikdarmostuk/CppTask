3cc59a0780d7c59c507c14e9d634ff903a8f8efc
vadikdarmostuk <90718027+vadikdarmostuk@users.noreply.github.com>
2021-11-30 20:41:55 +0200

    Add files via upload

---
+++ homeworck 2.11.c
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int a,v,s,t,t2;
+   cin >> a;
+   cin >> t;
+   s = (t*t*a)/2;
+   cout <<"s = "<<s;
+   cin >> v;
+   t2 = v/a;
+   cout <<"t="<<t2;
+    return 0;
+}
