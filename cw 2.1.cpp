35cb0ba31f2ea2cccd53cfc9b04978317bfa6115
vadikdarmostuk <90718027+vadikdarmostuk@users.noreply.github.com>
2021-12-07 23:42:23 +0200

    Дармостук Вадим компмат1
    Дармостук Вадим компмат1 CW 2

---
+++ cw 2.1.cpp
@@ -0,0 +1,13 @@
+
+
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    float a;
+    printf("a=\n");
+    scanf("%f", &a);
+    printf("cos(a)=%f",cos(a));
+    return 0;
+}
---
+++ cw 2.2.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    float a,b;
+    printf("a=\n");
+    scanf("%f", &a);
+    printf("b=\n");
+    scanf("%f", &b);
+    printf("c=%f",sqrt(a*a+b*b));
+    return 0;
+}
+
