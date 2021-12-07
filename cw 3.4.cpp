7abc58d1964d5f18635c3c617a1cf720ef7c159c
vadikdarmostuk <90718027+vadikdarmostuk@users.noreply.github.com>
2021-12-08 00:27:44 +0200

    Дармостук Вадим компмат1 CW 3
    Дармостук Вадим компмат1 CW 3

---
+++ cw 3.4.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+#include <stdio.h>
+#include <float.h>
+using namespace std;
+uint16_t dob(uint8_t x, uint8_t y)
+{
+    return x*y;
+}
+
+int main() {
+    printf(" %d\n",dob(203, 179));
+}
